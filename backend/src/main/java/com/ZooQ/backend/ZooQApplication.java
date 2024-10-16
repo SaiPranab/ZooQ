ad2a5b3cd12e56f92d2f871567e6bc0ff5a5159c
Sai <saipranabpatra5233@gmail.com>
2024-10-16 10:02:32 +0530

    initial commit

---
+++ backend/src/main/java/com/ZooQ/backend/ZooQApplication.java
@@ -0,0 +1,13 @@
+package com.ZooQ.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooQApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ZooQ/backend/ZooQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZooQ.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
