d6c62ab0bb64a15f0c2ea7a3237ac8ffd7632d49
Sai <saipranabpatra5233@gmail.com>
2024-10-17 07:59:30 +0530

    add controller, service, model for Category

---
+++ backend/src/main/java/com/ZooQ/backend/model/Category.java
@@ -0,0 +1,11 @@
+package com.ZooQ.backend.model;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+
+}
---
+++ backend/src/main/java/com/ZooQ/backend/service/ICategoryService.java
@@ -0,0 +1,14 @@
+package com.ZooQ.backend.service;
+
+import com.ZooQ.backend.model.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+     CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+     CategoryDTO createCategory(CategoryDTO categoryDTO);
+
+     CategoryDTO deleteCategory(Long categoryId);
+
+     CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
+}
